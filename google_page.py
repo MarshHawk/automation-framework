4a9c5abb3d0dd5907a070d0a64c8b7ed8426ca28
Sean Marsh Glover <s.glover12@gmail.com>
2018-12-19 23:41:00 -0500

    make web elements private

--- google_page.py
+++ google_page.py
@@ -4,22 +4,22 @@ class GooglePage(BasePage):
 
     #Web elements:
 
-    def get_search_box(self):
+    def __get_search_box(self):
         return self.driver.find_element_by_css_selector("input[title='Search'")
 
-    def get_search_button(self):
+    def __get_search_button(self):
         return self.driver.find_element_by_css_selector("input[name='btnK'")
     
     #Page actions:
 
     def get_search_box_text(self):
-        return self.get_search_box().get_attribute('value')
+        return self.__get_search_box().get_attribute('value')
 
     def set_search_box_text(self, text_to_search):
-        self.get_search_box().send_keys(text_to_search)
+        self.__get_search_box().send_keys(text_to_search)
 
     def click_search(self):
-        self.get_search_box().click()
+        self.__get_search_box().click()
 
     #Page conditions:
 
--- test_google.py
+++ test_google.py
@@ -23,7 +23,6 @@ def test_google_search_button(driver):
     google_page = GooglePage(driver)
     expected_search_text = "True Fit"
     google_page.set_search_box_text(expected_search_text)
-    actual_search_text = google_page.get_search_box_text()
-    log.info('actual search text: {0}'.format(actual_search_text))
-    assert actual_search_text, expected_search_text        
+    google_page.click_search()
+    #TODO: assert on page       
     driver.close()
